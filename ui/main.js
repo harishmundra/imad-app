37dacc4694f339a8345b958b11b989626831c017
harishmundra <30972948+harishmundra@users.noreply.github.com>
2017-08-15 15:15:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 
 // Changing the value of HTML text displayed on the screen
 
-var element = getElementById('main-text');
+var element = document.getElementById('main-text');
 element.innerHTML = "Changing the value of HTML from Java Script";
