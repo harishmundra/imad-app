10bd15d1874cabf0a4245c1dd4926690776e1078
harishmundra <30972948+harishmundra@users.noreply.github.com>
2017-08-16 21:10:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,22 @@ console.log('Loaded!');
  WILL BE WRITING ANOTHER PIECE OF CODE HERE 
   
   */
+  
+  
+  // Code to call the Counter iincrement from server.js
+  
+  var button = document.getElementById('counter');
+  var counter = 0;
+  
+  button.onClick = function(){
+      
+      // Make a request to the counter increment code
+      
+      // Capture the response and store it in a variable
+      
+      // Render the output in correct span
+      counter++;
+      var span = document.getElementById('count');
+      span.innerHTML = counter.toString();
+      
+  };
