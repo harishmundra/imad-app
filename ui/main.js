5b4ac3685dc4d2a8b5a613e0e7ed99d4df384a65
harishmundra <30972948+harishmundra@users.noreply.github.com>
2017-08-16 20:50:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,24 @@
 console.log('Loaded!');
 
+/*
 // Changing the value of HTML text displayed on the screen
 
-// var element = document.getElementById('main-text');
-// element.innerHTML = "Changing the value of HTML from Java Script";
+ var element = document.getElementById('main-text');
+ element.innerHTML = "Changing the value of HTML from Java Script";
 
 // Move the image on click
-// var img = document.getElementById('madi');
-// var marginLeft = 0;
+ var img = document.getElementById('madi');
+ var marginLeft = 0;
 
-// function moveRight(){
-//     marginLeft = marginLeft + 1;
-    // img.style.marginLeft = marginLeft + 'px';
-// }
+ function moveRight(){
+     marginLeft = marginLeft + 1;
+ img.style.marginLeft = marginLeft + 'px';
+ }
 
-// img.onclick = function() {
-// var interval = setInterval(moveRight,500);
-//  };
+ img.onclick = function() {
+ var interval = setInterval(moveRight,500);
+  };
+  
+ WILL BE WRITING ANOTHER PIECE OF CODE HERE 
+  
+  */
