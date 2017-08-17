2a608df630857ac884a7a9f0098d015ff83020ed
harishmundra <30972948+harishmundra@users.noreply.github.com>
2017-08-17 19:23:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/counter', function(req, res){
 });
 
 var names = [];
-app.get('/submit-name/', function(req, res) { //to be of form /submit-name/?name=xxxx
+app.get('/submit-name', function(req, res) { 
     var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
