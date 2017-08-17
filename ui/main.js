c75adabd00b9942b3e9be28d20eec14134381c54
harishmundra <30972948+harishmundra@users.noreply.github.com>
2017-08-17 19:49:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -92,7 +92,7 @@ console.log('Loaded!');
 
     // Make a request
     
-      request.open('GET', 'http://harishmundra.imad.hasura-app.io/submit-name', true);
+      request.open('GET', 'http://harishmundra.imad.hasura-app.io/submit-name?name= ' + name1, true);
       request.send(ul);
       
       
