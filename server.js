11a03145353d6f9ad7ae6989415a5c3765df3e08
harishmundra <30972948+harishmundra@users.noreply.github.com>
2017-08-20 21:17:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.get('/articles/:articleName', function(req, res){
     var articleName = req.params.articleName;
     //res.send(createTemplate(articles[articleName])); retaining from the html page handling
     
-    pool.query("select * from article where title = " + "'" + req.params.articleName + "'", function(err, result){
+    pool.query("select * from article where title = $1" + [req.params.articleName], function(err, result){
        
        if (err){
            res.status(500).send(err.toString());
